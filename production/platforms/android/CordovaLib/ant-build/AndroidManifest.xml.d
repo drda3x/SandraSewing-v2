39dab170fb9ee4e134783496cba23eb0969c2f34
Nesterov <da3x11@gmail.com>
2014-06-05 12:05:19 +0400

    Что-то страшное!

---
+++ production/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\Da3x\My_projects\SandraSewing-v2\production\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+D:\Da3x\My_projects\SandraSewing-v2\production\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ production/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : D:\Da3x\My_projects\SandraSewing-v2\production\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
