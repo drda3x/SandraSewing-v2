bf48908eae7c5763a1024ea224aa5d4bf8855397
Nesterov <da3x11@gmail.com>
2014-06-11 17:02:59 +0400

    Новая ветка для разработки приложения и изучения ангулара

--- static/js/ss_logic.js
+++ static/js/ss_logic.js
@@ -1,39 +1,16 @@
-/**
- * Created by al on 05.06.14.
- *
- * Логика работы ресурса
- *
- *
- * '<ul id="dimensions">' +
- '<li class="current_dim hover_class" ng-repeat="dim in dimensions">{{ dim }}' +
- '<div class="sub_menu">' +
- '<img class="sub_menu_list" src="img/pen.png " />' +
- '<img class="sub_menu_list" src="img/x.png " />' +
- '</div>'
- */
+(function() {
+    var app = angular.module('s_sewing', []);
 
-angular.module('s_sewing',[])
-    .directive('body', function() {
-            return {
-                restrict: 'E',
-                scope: {},
-                controller: function($scope, $element) {
-                    $scope.openAddWindow = function() {
-                        $element.append('<div appendedDiv ng-click="appendedFunc()"></div>');
-                    }
-                },
-                link: function(scope, element, attrs) {
-                    var a = element;
+    app.directive('mainDirective', function() {
+        return {
+            restrict: 'A',
+            controller: function($scope) {
+                $scope.currentWindow = 'main';
+
+                $scope.changeWindow = function(window) {
+                    $scope.currentWindow = window;
                 }
             }
-        })
-
-function DimensionsCtrl($scope) {
-    $scope.dimensions = [
-        {name: 'Мерка 1'},
-        {name: 'Мерка 2'},
-        {name: 'Мерка 3'}
-    ]
-    $scope.last_dimension = $scope.dimensions[$scope.dimensions.length-1];
-    $scope.dimensions.length--;
-}
+        };
+    });
+})();
