671d32977c06ff8a4617b70bae2b325a255aeed2
drda3x <da3x11@gmail.com>
2014-06-12 02:31:44 +0400

    Ура!!! Наконец-то получилось!!!

--- static/js/ss_logic.js
+++ static/js/ss_logic.js
@@ -1,6 +1,7 @@
 (function() {
     var app = angular.module('s_sewing', []);
 
+    // Директивы
     app.directive('mainDirective', function() {
         return {
             restrict: 'A',
@@ -12,7 +13,43 @@
                         this.currentWindow = window;
                     }
                 };
+
+                $scope.test = 'dim';
             }
         };
     });
+
+    app.directive('dimensions', function() {
+        return {
+            restrict: 'E',
+            controller: function($scope) {
+                $scope.dimensions_menu = {
+                    items: [
+                        {name: 'Мерка 1'},
+                        {name: 'Мерка 2'},
+                        {name: 'Мерка 3'}
+                    ],
+                    render_conf: {
+                        c_index: 0,
+                        isLast: function() {
+                            if (this.c_index == $scope.dimensions_menu.items.length-1) {
+                                this.c_index = 0;
+                                return 'last_dim';
+                            } else {
+                                this.c_index++;
+                            }
+                        },
+                        addNewItem: function() {}
+                    }
+                };
+
+            },
+            controllerAs: 'dimCtrl',
+            templateUrl: './templates/dimension_menu.html',
+            replace: true
+        }
+    });
+
+    // Контроллеры
+
 })();
