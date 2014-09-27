89035a18c6b58e42236a1440f18a4d59a63794f3
drda3x <da3x11@gmail.com>
2014-09-27 13:01:18 +0400

    Переделываем все!

--- static/js/directives.js
+++ static/js/directives.js
@@ -5,28 +5,6 @@
 (function(window) {
     var app = angular.module('s_sewing', []);
 
-    var global = {
-        dimensions: {
-            // Список мерок
-            items: window.initial_data.test_dimensions,
-            
-            // Текущая мерка
-            currentItem: null,
-
-            // Метод для создания новой мерки
-            create: function(name, type, values) {
-                this.items.push(
-                    new window.defaultStructures.defDimansionItemStructure(
-                        this.items.length + 1,
-                        name,
-                        type,
-                        values
-                    )
-                );
-            }
-        }
-    };
-
     // Директивы
     // Директива поведения главного контейнера (body)
     app.directive('mainDirective', function() {
@@ -34,14 +12,13 @@
             restrict: 'A',
             controller: function($scope) {
 
+                $scope.dimensions.items = window.initial_data.test_dimensions;
+
                 $scope.appWindows = {
                     currentWindow: 'main',
                     changeCurrent: function(window, item) {
                         this.currentWindow = window;
-                        global.dimensions.currentItem = item;
-                        if(global.Ctrls.dimensionWindowCtrl) {
-                            global.Ctrls.dimensionWindowCtrl.init(item);
-                        }
+                        $scope.dimensions.currentItem = item;
                     }
                 };
 
@@ -55,36 +32,28 @@
         return {
             restrict: 'E',
             controller: function($scope) {
-                $scope.dimensionsMenuCtrl = {
 
-                    // Список - мерки
-                    items: global.dimensions.items,
+                $scope.selectedItem = null;
 
-                    // Объект для манипуляций со списком
-                    render_conf: {
-                        // Метод для удаления элементов
-                        removeItem: function(item) {
-                            var items = $scope.dimensionsMenuCtrl.items;
+                $scope.select = function(item) {
+                    this.selectedItem = item.id;
+                };
 
-                            // Проходим по списку и удаляем нужный нам элемент
-                            for(var i= 0, j= items.length; i<j; i++) {
-                                if(items[i].id == item.id) {
-                                    items.splice(i,1);
-                                    break;
-                                }
-                            }
-                        },
-                        selectedItem: null,
-                        select: function(item) {
-                            this.selectedItem = item.id;
-                        },
-                        isSelected: function(item) {
-                            var b = item.id == this.selectedItem;
-                            return b;
+                $scope.isSelected = function(item) {
+                    return item.id == this.selectedItem;
+                };
+
+                $scope.removeItem = function(item) {
+                    var items = $scope.items;
+
+                    // Проходим по списку и удаляем нужный нам элемент
+                    for(var i= 0, j= items.length; i<j; i++) {
+                        if(items[i].id == item.id) {
+                            items.splice(i,1);
+                            break;
                         }
                     }
                 };
-
             },
             controllerAs: 'dimCtrl',
             templateUrl: 'templates/dimension_menu.html',
@@ -92,84 +61,30 @@
         }
     });
 
-    var dimWindowCtrl = {
-        dimensions: global.dimensions,
-        c_name: ((global.dimensions.currentItem) ? global.dimensions.currentItem.name : null),
-        create_or_update: function() {
-            if(this.dimensions.currentItem && !this.dimensions.currentItem.id && this.dimensions.currentItem.name.length > 0) {
-                this.dimensions.currentItem.id = this.dimensions.items.length+1;
-                this.dimensions.items.push(this.dimensions.currentItem);
-            }
-
-            return true;
-        }
-    };
-
-/*    var dimWinCtrl = {
-        dimensions: global.dimensions,
-        create_or_update: function() {
-            if(this.dimensions.currentItem && !this.dimensions.currentItem.id && this.dimensions.currentItem.name.length > 0) {
-                this.dimensions.currentItem.id = this.dimensions.items.length+1;
-                this.dimensions.items.push(this.dimensions.currentItem);
-            }
-
-            return true;
-        }
-    };*/
-
-    var dimWinCtrl = {
-        models: {
-            itemName: null,
-            item: null
-        },
-        init: function(item) {
-            this.models.itemName = (item) ? item.name : null;
-            this.models.item = item;
-        },
-        create_or_update: function() {
-            if(this.models.itemName && this.models.itemName.length > 0) {
-                if(!this.models.item) {
-                    global.dimensions.create(this.models.itemName);
-                } else {
-                    this.models.item.name = this.models.itemName;
-                }
-            }
-            return true;
-        }
-    };
-
     app.directive('newDimensionWindow', function() {
         return {
             restrict: 'E',
             templateUrl: 'templates/dimension_window.html',
             controller: function($scope) {
-                var dimWindowCtrl = {
-                    models: {
-                        itemName: null,
-                        item: null
-                    },
-                    init: function(item) {
-                        this.models.itemName = (item) ? item.name : null;
-                        this.models.item = item;
-                    },
-                    create_or_update: function() {
-                        if(this.models.itemName && this.models.itemName.length > 0) {
-                            if(!this.models.item) {
-                                global.dimensions.create(this.models.itemName);
-                            } else {
-                                this.models.item.name = this.models.itemName;
-                            }
-                        }
-                        return true;
-                    }
+
+                $scope.itemName = null;
+                $scope.item = null;
+
+                $scope.init = function(item) {
+                    this.models.itemName = (item) ? item.name : null;
+                    this.models.item = item;
                 };
 
-                if(!global.Ctrls) {
-                    global.Ctrls = {}
-                }
-
-                global.Ctrls.dimensionWindowCtrl = dimWindowCtrl;
-                $scope.dimWindowCtrl = dimWindowCtrl;
+                $scope.create_or_update = function(item) {
+                    if(this.models.itemName && this.models.itemName.length > 0) {
+                        if(!this.models.item) {
+                            global.dimensions.create(this.models.itemName);
+                        } else {
+                            this.models.item.name = this.models.itemName;
+                        }
+                    }
+                    return true;
+                };
             }
         };
     });
