de4abe7a613e8eb1355df634d3ae0da2c2f86ad6
Nesterov <da3x11@gmail.com>
2014-10-14 12:01:32 +0400

    Закончена верстка окна с расчетами

--- production/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Da3x\My_projects\SandraSewing-v2\production\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\Da3x\My_projects\SandraSewing-v2\production\platforms\android\CordovaLib\AndroidManifest.xml \
