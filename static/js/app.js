4604b388f1e77f58b278bff3cd507bf2cba0d689
Nesterov <da3x11@gmail.com>
2014-10-14 18:29:04 +0400

    Добавлено базовое поведение доп. экрана

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,38 @@
  */
 
 (function() {
-    var app = angular.module('sandra_sewing', []);
+    var app = angular.module('sewing_app', []);
+
+    /**
+     * Показываем или скрываем доп. экран
+     */
+    app.controller('showsSubScreenCtrl', ['$scope', function($scope) {
+        $scope.showSubScreen = false;
+
+        $scope.showOrHideSubScreen = function(name) {
+            $scope.showSubScreen = (($scope.showSubScreen == name) ? false : name);
+        }
+    }]);
+
+    app.directive('subScreen', function() {
+        return {
+            restrict: 'A',
+            templateUrl: './templates/sub_screen_tmp.html'
+        }
+    });
+
+    app.directive('screen', function() {
+        return {
+            restrict: 'E',
+            require: '^screensList',
+            controller: function($scope) {
+                $scope.isAvailable = function(name) {
+                    return name == $scope.showSubScreen;
+                }
+            },
+            template: '<span ng-transclude></span>',
+            transclude: true
+        }
+    });
+
 })();
