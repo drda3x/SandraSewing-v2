85b5de3a3203661bfaf813a203208825945c0da7
Vasily Nesterov <da3x11@gmail.com>
2014-11-24 10:51:58 +0300

    Начата работа над логикой ветвления алгоритма

--- static/js/test.js
+++ static/js/test.js
@@ -7,22 +7,18 @@
     var testAlg = new api.Algorithm('HAAALK');
 
     var step0 = new api.Step(
-            '<div>Введите число<input type="text" ng-model="stepValues[\'inp_1\']"></div>'
+            '<div>Требуется ли продолжить работу алгоритма? </div>' +
+            '<input type="button" value="Нет"> ' +
+            '<input type="button" value="Да">'
         ),
         step1 = new api.Step(
             '<div>Я результат шага 1: <<val_1>></div>',
             new api.Value('val_1', 'inp_1', function() {
-                return parseInt(arguments[0]) + 1;
-            })
-        ),
-        step2 = new api.Step(
-            '<div>Я - результат шага 2 <<val_1>> <<val_2>></div>',
-            new api.Value('val_2', 'val_1', function(req){
-                return req[0] * 2;
+                return 1 + 1;
             })
         );
 
-    testAlg.addSteps([step0, step1, step2]);
+    testAlg.addSteps([step0, step1]);
 
     var t = 1;
 
