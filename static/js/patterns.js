701a7f7aab3c1021f589bd7c4ae621b3419c9720
Vasily Nesterov <da3x11@gmail.com>
2015-02-14 13:11:51 +0300

    Новая концепция алгоритма

--- static/js/patterns.js
+++ static/js/patterns.js
@@ -1,6 +1,27 @@
 /**
- * Код алгоритмов выкроек и тд...
+ * Алгоритмы выкроек
  */
 (function() {
+    /**
+     * Выкройка брюк
+     * @constructor
+     */
+    function Panes() {
+        this.algorithm = [];
+    }
 
+    // Переход к следующему шагу алгоритма
+    Panes.prototype.next = function() {}
+
+    // Переход к предыдущему шагу алгоритма
+    Panes.prototype.prev = function() {}
+
+    // Сброс алгоритма на начало
+    Panse.prototype.reset = function() {}
+
+    // Загрузка размеров
+    Panes.prototype.config = function(dim) {
+        this.pob = dim.pob;
+        this.di = dim.di;
+    }
 })();
